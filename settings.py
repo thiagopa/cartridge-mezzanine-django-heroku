09478ce23a648834832c0cda3c11294e6d1a3d52
Thiago Pagonha <thi.pag@gmail.com>
2013-11-11 13:53:25 -0200

    Modificando o settings pra funcionar localmente e copiando os thumbnails

--- settings.py
+++ settings.py
@@ -367,19 +367,6 @@ OPTIONAL_APPS = (
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False}
 
 
-##################
-# LOCAL SETTINGS #
-##################
-
-# Allow any settings to be defined in local_settings.py which should be
-# ignored in your version control system allowing for settings to be
-# defined per machine.
-try:
-    from local_settings import *
-except ImportError:
-    pass
-
-
 ####################
 # DYNAMIC SETTINGS #
 ####################
@@ -412,3 +399,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+##################
+# LOCAL SETTINGS #
+##################
+
+# Allow any settings to be defined in local_settings.py which should be
+# ignored in your version control system allowing for settings to be
+# defined per machine.
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
