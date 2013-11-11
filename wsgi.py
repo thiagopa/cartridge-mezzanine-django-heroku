c5344ae54d16379cb2807598c0810958057428cf
Thiago Pagonha <thi.pag@gmail.com>
2013-11-11 12:40:12 -0200

    novos medias

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,5 @@ settings_module = "settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+from dj_static import Cling, MediaCling
+application = Cling(MediaCling(get_wsgi_application()))
