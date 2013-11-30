399498ced52e29579546b78c92ba3108cd1db207
Thiago Pagonha <thi.pag@gmail.com>
2013-11-30 17:48:46 -0200

    Paypal não aceita integração direta com cartão de crédito no brasil

--- integration/checkout.py
+++ integration/checkout.py
@@ -4,6 +4,10 @@
 Checkout process customization.
 """
 
+import logging, locale
+
+logger = logging.getLogger("integration.checkout")
+
 from django.utils.translation import ugettext as _
 
 from mezzanine.conf import settings
@@ -13,36 +17,115 @@ from cartridge.shop.utils import set_shipping
 
 from suds.client import Client
 from decimal import Decimal
+from cartridge.shop.checkout import CheckoutError
 
-def default_billship_handler(request, order_form):
-    """
-    Default billing/shipping handler - called when the first step in
-    the checkout process with billing/shipping address fields is
-    submitted. Implement your own and specify the path to import it
-    from via the setting ``SHOP_HANDLER_BILLING_SHIPPING``.
-    This function will typically contain any shipping calculation
-    where the shipping amount can then be set using the function
-    ``cartridge.shop.utils.set_shipping``. The Cart object is also
-    accessible via ``request.cart``
-    """
-    if not request.session.get("free_shipping"):
-        settings.use_editable()
+import paypalrestsdk
+from paypalrestsdk import Payment
+
+def correios_billship_handler(request, order_form):
+	"""
+	Default billing/shipping handler - called when the first step in
+	the checkout process with billing/shipping address fields is
+	submitted. Implement your own and specify the path to import it
+	from via the setting ``SHOP_HANDLER_BILLING_SHIPPING``.
+	This function will typically contain any shipping calculation
+	where the shipping amount can then be set using the function
+	``cartridge.shop.utils.set_shipping``. The Cart object is also
+	accessible via ``request.cart``
+	"""
+	logger.debug("integration.checkout.correios_billship_handler()")
+	if not request.session.get("free_shipping"):
+        	settings.use_editable()
         
-	# http://www.correios.com.br/webservices/default.cfm
-	# http://www.correios.com.br/webServices/PDF/SCPP_manual_implementacao_calculo_remoto_de_precos_e_prazos.pdf
+		# http://www.correios.com.br/webservices/default.cfm
+		# http://www.correios.com.br/webServices/PDF/SCPP_manual_implementacao_calculo_remoto_de_precos_e_prazos.pdf
 
-	client = Client("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl")
+		client = Client("http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl")
 	
+		"""
+			40010 = SEDEX
+		"""
+		origin_cep = settings.CEP_ORIGIN
+		data = order_form.cleaned_data
+		dest_cep = data["billing_detail_postcode"]
+
+		logger.debug("cep de %s para %s" % (origin_cep,dest_cep) )
+		# Propriedades fixas apenas para testar por enquanto
+		result = client.service.CalcPreco("","",40010, origin_cep, dest_cep , "1.0",1, 50.0, 50.0, 50.0, 50.0, "N", 0 , "N")
+
+		price = Decimal(result.Servicos[0][0].Valor.replace(",","."))
+		logger.debug("preço %10.2d" % price)
+
+		set_shipping(request, _("SEDEX"), price)
+
+def paypal_payment_handler(request, order_form, order):
 	"""
-		40010 = SEDEX
+	Default payment handler - called when the final step of the
+	checkout process with payment information is submitted. Implement
+	your own and specify the path to import it from via the setting
+	``SHOP_HANDLER_PAYMENT``. This function will typically contain
+	integration with a payment gateway. Raise
+	cartridge.shop.checkout.CheckoutError("error message") if payment
+	is unsuccessful.
 	"""
-	origin_cep = settings.CEP_ORIGIN
+
+	logger.debug("integration.checkout.paypal_payment_handler()")
+   
+	logger.debug("request %s \n order_form %s \n order %s" % (request, order_form, order) )
+ 
+	try:
+		PAYPAL_CLIENT_ID = settings.PAYPAL_CLIENT_ID
+		PAYPAL_SECRET = settings.PAYPAL_SECRET
+	except AttributeError:
+		raise ImproperlyConfigured(_("Credenciais de acesso ao paypal estão faltando, "
+				"isso inclui PAYPAL_CLIENT_ID e PAYPAL_SECRET "
+				"basta incluí-las no settings.py para serem utilizadas "
+				"no processador de pagamentos do paypal."))
+
+	if settings.DEBUG:
+		mode = "sandbox"
+	else:
+		mode = "live"
+
+	api = paypalrestsdk.set_config(
+		mode = mode, # sandbox or live
+		client_id = PAYPAL_CLIENT_ID,
+		client_secret = PAYPAL_SECRET
+	)
+	# autenticação
+	access_token = api.get_token()
+
+	logger.debug("Paypal Access Token %s" % access_token)
+
 	data = order_form.cleaned_data
-	dest_cep = data["billing_detail_postcode"]
+	locale.setlocale(locale.LC_ALL, settings.SHOP_CURRENCY_LOCALE)
+	currency = locale.localeconv()
+	currency_code = currency['int_curr_symbol'][0:3] 
+	logger.debug("Currency Code %s" % currency_code)
 
-	result = client.service.CalcPreco("","",40010, origin_cep, dest_cep , "1.0",1, 50.0, 50.0, 50.0, 50.0, "N", 0 , "N")
+	payment = Payment({
+		"intent": "sale",
+		"payer": {
+			"payment_method": "paypal",
+		},
+		"redirect_urls" : {
+			"return_url" : "http://localhost:5000/payment/execute",
+			"cancel_url" : "http://localhost:5000"
+		},
+		"transactions": [{
+			"amount": {
+				"total": str(order.total),
+				"currency": currency_code
+			},
+			"description": "Compra de Produtos na loja." 
+    		}]
+	})
 
-	price = Decimal(result.Servicos[0][0].Valor.replace(",","."))
-
-	set_shipping(request, _("SEDEX"), price)
+	if payment.create():
+		logger.debug("Payment[%s] created successfully"%(payment.id))
+		return payment.id
+	else:
+		# Display Error message
+		logger.error("Payment error \n %s" % payment)
+		raise CheckoutError(payment.error)
 
--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['*']
 # out, for convenient overriding.
 
 # Sequence of available credit card types for payment.
-SHOP_CARD_TYPES = ("Mastercard", "Visa")
+# SHOP_CARD_TYPES = ("Mastercard", "Visa")
 
 # Setting to turn on featured images for shop categories. Defaults to False.
 # SHOP_CATEGORY_USE_FEATURED_IMAGE = True
@@ -22,11 +22,11 @@ SHOP_CARD_TYPES = ("Mastercard", "Visa")
 
 # If True, the checkout process is split into separate
 # billing/shipping and payment steps.
-# SHOP_CHECKOUT_STEPS_SPLIT = True
+SHOP_CHECKOUT_STEPS_SPLIT = True
 
 # If True, the checkout process has a final confirmation step before
 # completion.
-# SHOP_CHECKOUT_STEPS_CONFIRMATION = True
+SHOP_CHECKOUT_STEPS_CONFIRMATION = True
 
 # Controls the formatting of monetary values accord to the locale
 # module in the python standard library. If an empty string is
@@ -37,7 +37,7 @@ SHOP_CURRENCY_LOCALE = "pt_BR.UTF-8"
 # is called on submit of the billing/shipping checkout step. This
 # is where shipping calculation can be performed and set using the
 # function ``cartridge.shop.utils.set_shipping``.
-SHOP_HANDLER_BILLING_SHIPPING = "integration.checkout.default_billship_handler"
+SHOP_HANDLER_BILLING_SHIPPING = "integration.checkout.correios_billship_handler"
 
 # Dotted package path and class name of the function that
 # is called once an order is successful and all of the order
@@ -48,13 +48,16 @@ SHOP_HANDLER_BILLING_SHIPPING = "integration.checkout.default_billship_handler"
 # Dotted package path and class name of the function that
 # is called on submit of the payment checkout step. This is where
 # integration with a payment gateway should be implemented.
-SHOP_HANDLER_PAYMENT = "integration.paypal.process"
+SHOP_HANDLER_PAYMENT = "integration.checkout.paypal_payment_handler"
 
-# Sequence of value/name pairs for order statuses.
-# SHOP_ORDER_STATUS_CHOICES = (
-#     (1, "Unprocessed"),
-#     (2, "Processed"),
-# )
+SHOP_ORDER_STATUS_CHOICES = (
+     (1, "Não Processado"),
+     (2, "Pendente de Pagamento"),
+     (3, "Pago, Falta Enviar"),
+     (4, "Enviado"),
+     (5, "Entregue")
+
+ )
 
 # Sequence of value/name pairs for types of product options,
 # eg Size, Colour.
@@ -417,9 +420,12 @@ except ImportError:
 # PayPal Credentials
 #import os
 
-PAYPAL_USER = os.environ["PAYPAL_USER"]
-PAYPAL_PASSWORD = os.environ["PAYPAL_PASSWORD"]
-PAYPAL_SIGNATURE = os.environ["PAYPAL_SIGNATURE"]
+#PAYPAL_USER = os.environ["PAYPAL_USER"]
+#PAYPAL_PASSWORD = os.environ["PAYPAL_PASSWORD"]
+#PAYPAL_SIGNATURE = os.environ["PAYPAL_SIGNATURE"]
+
+PAYPAL_CLIENT_ID = os.environ["PAYPAL_CLIENT_ID"]
+PAYPAL_SECRET = os.environ["PAYPAL_SECRET"]
 
 LOGGING = {
     'version': 1,
@@ -455,7 +461,7 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': False,
         },
-        'integration.paypal': {
+        'integration': {
             'handlers': ['console', 'mail_admins'],
             'level': 'DEBUG',
         }
